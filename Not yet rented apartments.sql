0acdc5e71abb5eea893ff89257291d0c85f10755
Caleb <48107964+Caleb-C004@users.noreply.github.com>
2022-05-13 10:46:36 -0500

    Delete Not yet rented apartments.sql

--- Not yet rented apartments.sql
+++
@@ -1,2 +0,0 @@
-SELECT aptNum, direction, nickname, CAST(MOD(aptNum / 100, 10) AS INT) floorNum, rentPrice, bathColor, carColor carpetColor, kitAppColor
-FROM buildings NATURAL JOIN apartment NATURAL JOIN emptyAppt NATURAL JOIN rentPrice;

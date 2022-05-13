2903738a02ce0b8caac7fcaee00384c11b64abe5
Caleb <48107964+Caleb-C004@users.noreply.github.com>
2022-05-13 10:47:33 -0500

    Delete available newer apartments.sql

--- available newer apartments.sql
+++
@@ -1,6 +0,0 @@
-SELECT val, nickname, COUNT(aptNum) openApartments
-FROM buildings LEFT JOIN renBuilding ON buildings.buildNumber = renBuilding.buildNumber 
-LEFT JOIN newBuilding ON buildings.buildNumber = newBuilding.buildNumber 
-LEFT JOIN emptyAppt ON buildings.buildNumber = emptyAppt.buildNumber
-WHERE renYear > 2019 OR constYear > 2019 
-GROUP BY val, nickname;
